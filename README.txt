32c71ec7db4c8071a7b6a52c8e1fd907f1d67900
Patrick <pertriquek171@gmail.com>
2018-05-02 14:03:42 -0400

    Final changes

---
+++ README.txt
@@ -0,0 +1,12 @@
+Author: Patrick Kyoyetera
+
+
+These are the sources I have used iduring the implemenation of this project.
+
+
+1. https://www.tutorialspoint.com/computer_graphics/3d_transformation.htm (for transformations)
+2. https://workshop.chromeexperiments.com/examples/gui/  (for menu) 
+3. https://threejs.org/docs/ (library used for all implemenation)
+4. http://www.opengl-tutorial.org/beginners-tutorials/tutorial-3-matrices/ (Affine Matrix transformations)
+5. http://www.euclideanspace.com/maths/geometry/affine/matrix4x4/ (More matrix stuff)
+
--- src/js/final.js
+++ src/js/final.js
@@ -1,7 +1,7 @@
 /*
 *   Author: Patrick Kyoyetera
 *   Description: Final Project Phase 4
-*   Date: Wed, April 18, 2018
+*   Date: Wed, May 2, 2018
 *
 */
 
@@ -38,30 +38,60 @@ function init() {
     var view_angle = 45, aspect_ratio = width/height;
     var camera = new THREE.PerspectiveCamera(view_angle, aspect_ratio, 0.1, 10000);
     camera.position.set(0, 150, 300);
-    camera.lookAt(scene.position);
+    //camera.lookAt(scene.position);
 
     // Keyboard controls 
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
     // lighting
-    var lighting = new THREE.PointLight(0xfffffe, 1, 20);
-    lighting.position.set(0, 250, 0);
-    scene.add(camera, lighting);
+    var light2 = new THREE.SpotLight(0xff0000);
+    light2.position.set(75, 120, 80);
+    light2.intensity = 7.5;
+
+    var light1 = new THREE.DirectionalLight(0xfffffe, .51)//, 50);
+    light1.position.set(75, 120, 80);
+    light1.intensity = 2.5;
+    light1.castShadow = true;
+    
+    
+    /*
+    light.shadow.mapSize.width = 512;  
+    light.shadow.mapSize.height = 512; 
+    light.shadow.camera.near = 0.5;    
+    light.shadow.camera.far = 500 
+
+    */
+    // bulb object
+    var bulb = new THREE.SphereGeometry(5, 25, 25);
+    var bulbFrame = objectFrame(bulb, 0x4F1133, 75, 120, 80);
+
+    scene.add(camera, light1, bulbFrame);
 
     // house object
-    var cubeMaterial = new THREE.MeshBasicMaterial( {color: 0x45ffee} ),
-        cylMaterial = new THREE.MeshBasicMaterial( {color: 0x5a0a40} ),
+    var cubeMaterial = new THREE.MeshPhongMaterial( {color: 0x45ffee} ),
+        cylMaterial = new THREE.MeshPhongMaterial( {color: 0x5a0a40} ),
         cylGeometry = new THREE.CylinderGeometry(25, 85, 33, 6),
         cubeGeo = new THREE.BoxGeometry(84, 85, 82);
+        
 
     var house = new THREE.Mesh(cubeGeo, cubeMaterial),
         house_frame = objectFrame(cubeGeo, 0x000000, 0, 0, 0);
 
     var roof = new THREE.Mesh(cylGeometry, cylMaterial);
+    roof.castShadow = true;
+    house,castShadow = true;
+
     roof.position.y = 50;
     var roof_frame = objectFrame(cylGeometry, 0x000000, roof.position.x, roof.position.y, roof.position.z);
 
     //scene.add(house, roof);
+
+    var planeGeometry = new THREE.PlaneBufferGeometry( 220, 220, 32, 32 );
+    var planeMaterial = new THREE.MeshPhysicalMaterial( { color: 0xFFFEFF } )
+    var floor = new THREE.Mesh( planeGeometry, planeMaterial );
+    floor.rotation.set(-Math.PI, 0, 0);
+    floor.receiveShadow = true;
+    //scene.add( plane );
 
     // group all house objects for easy control 
     var objGroup = new THREE.Group();
@@ -69,6 +99,8 @@ function init() {
     objGroup.add(house);
     objGroup.add(house_frame);
     objGroup.add( roof_frame);
+    //objGroup.add(floor);
+
     scene.add(objGroup);
 
     // keep object within window when window is resized
@@ -87,17 +119,18 @@ function init() {
 
     var params = { x: 0, y: 0, z: 0, 
                    x_axis:0, y_axis:0, z_axis: 0,
-                   v:1, h:1,
+                   translate: 0,                                   
                    xShear: 0, yShear: 0, zShear: 0,
                    scaleval: 0,
                    color: "ff004b",
-                   visible: true,
-                   material: "Phong",
-                   isometric: false, dimetric: false, trimetric: false,
+                   One_Point:false, Two_Point: false, Three_Point: false,
+                    Directional: false, Spot: false,
+                   isometric: false, dimetric: false, trimetric: false, vanishing: false,
                    reset: function() {
-                     resetObject();
+                     reset();
                     }
                  };
+    
 
     // add individual params to respective translation folders
     // position folder
@@ -119,14 +152,23 @@ function init() {
     var shearX = shear_folder.add(params, 'xShear').min(0).max(0.5).step(0.05).listen(),
         shearY = shear_folder.add(params, 'yShear').min(0).max(0.5).step(0.05).listen(),
         shearZ = shear_folder.add(params, 'zShear').min(0).max(0.5).step(0.05).listen();
-    shear_folder.open();
+    shear_folder.close();
 
     // projection folder 
     var projections_folder = menu.addFolder('Projections');
     var isometric = projections_folder.add(params, 'isometric').listen(),
         dimetric = projections_folder.add(params, 'dimetric').listen(),
         trimetric = projections_folder.add(params, 'trimetric').listen();
-    projections_folder.close();
+        vanishing = projections_folder.add(params, 'vanishing').listen();
+        oneP = projections_folder.add(params, 'One_Point').listen();
+        twoP = projections_folder.add(params, 'Two_Point').listen();
+        threeP = projections_folder.add(params, 'Three_Point').listen();
+    projections_folder.open();
+
+    var lightFolder = menu.addFolder('Lights: Directional and Spot');
+    var dir_light = lightFolder.add(params, 'Directional').listen(),
+        spot_light = lightFolder.add(params, 'Spot').listen();
+    lightFolder.open();
 
     // shear functionality 
     function shear(val, group, shearType) {
@@ -134,14 +176,20 @@ function init() {
             yShearMatrix = new THREE.Matrix4(),
             zShearMatrix = new THREE.Matrix4();
 
-        xShearMatrix.set(1, 0, 0, 0,
-                         val, 1, 0, 0,
+        xShearMatrix.set(1, val, val, 0,
+                         0, 1, 0, 0,
                          0, 0, 1, 0, 
                          0, 0, 0, 0 );
-        yShearMatrix.set(1, val, 0, 0,
+
+        yShearMatrix.set(1, 0, 0, 0,
+                         val, 1, val, 0,
+                         0, 0, 1, 0,
+                         0, 0, 0, 0);
+
+        zShearMatrix.set(1, 0, 0, 0,
                          0, 1, 0, 0,
-                         0, 0, 1, 0);
-        //zShearMatrix.set()
+                         val, val, 1, 0,
+                         0, 0, 0, 0);
 
         switch(shearType) {
             case 'x':
@@ -151,7 +199,8 @@ function init() {
                 group.applyMatrix(yShearMatrix);
                 break;
             case 'z':
-                alert("Yet to be implemented");
+                group.applyMatrix(zShearMatrix);
+            //alert("Yet to be implemented");
                 break;
             default:
                 alert("Go Home, you're drunk!");
@@ -159,11 +208,22 @@ function init() {
     }
     var count = 1;
 
-    // projections
-
     
     // scale option
     var scalearg = menu.add(params, 'scaleval').min(0).max(10).step(0.5).listen();
+
+    //translate
+    var trans = menu.add(params, 'translate').min(0).max(10).step(1).listen();
+
+    //var reset = menu.add(params, 'Reset').
+    
+    menu.add(params, 'reset');
+
+    function reset() {
+        clearScene();
+        scene.add(camera, light1, bulbFrame);
+        scene.add(objGroup);        
+    }
 
     // translations will be done here    
     var update = function() {
@@ -196,21 +256,39 @@ function init() {
         shearY.onChange( function (value) {
             shear(value, objGroup, 'y');
         });
+        shearZ.onChange( function (value) {
+            shear(value, objGroup, 'z');
+        });
 
         // scale object
         scalearg.onChange(function (value) {
             objGroup.scale.set(value, value, value);
+        });
+        
+        // translate
+        trans.onChange(function(value) {
+            var mat = new THREE.Matrix4();
+            mat.set(1, 0, 0, value, 
+                    0, 1, 0, value, 
+                    0, 0, 1, value,
+                    0, 0, 0, 1);
+
+            objGroup.applyMatrix(mat);
         });
 
         // projections 
         isometric.onChange(function () {
             if(true) {
                 var matrix = new THREE.Matrix4();
-                matrix.set(1, 0, -1, 0, 
-                           0, 1, -1, 0, 
-                           0, 0, 1, 0, 
-                           0, 0, 0, 1);
-    
+                /*matrix.set(Math.cos(Math.PI/6), -Math.cos(Math.PI/6),  0, 0, 
+                           Math.sin(Math.PI/6),  Math.sin(Math.PI/6),  1, 0, 
+                                             0,                    0,  0, 0, 
+                                             0,                    0,  0, 1);
+                */
+                matrix.set(1, 0, -1, 0,
+                           0, 1, -1, 0,
+                           0, 0,  1, 0,
+                           0, 0,  0, 1);
                 objGroup.applyMatrix(matrix);
                 //camera.position.set(220, 220, 220);
                 //camera.lookAt(scene.position);
@@ -222,10 +300,13 @@ function init() {
                 var matrix = new THREE.Matrix4();
                 matrix.set(1, 0, -1, 0, 
                            0, 1, 0, 0, 
-                           0, 0, 1, 0, 
+                           0, 0, -1, 0, 
                            0, 0, 0, 1);
     
                 objGroup.applyMatrix(matrix);
+                
+               //matrix.makePerspective(1, 1, 1, 1, 0.1, 1000);
+               //objGroup.applyMatrix(matrix);
             }
         });
         
@@ -241,18 +322,57 @@ function init() {
             }
         });
 
-        
+        vanishing.onChange(function() {
+            //var camera = new THREE.PerspectiveCamera(120, width/height, 0.1, 1000);
+            camera.position.x = -50;
+            camera.position.y = 100;
+            camera.position.z = -100;
+            camera.rotation.y = -45 * Math.PI / 180 ;
+        });
 
+        oneP.onChange(function() {
+            camera.position.x = 0;
+            camera.position.y = 0;
+            camera.position.z = 400;
+        });
+
+        twoP.onChange(function() {
+            camera.position.x = -400;
+            camera.position.y = 0;
+            camera.position.z = 400;
+        });
+
+        threeP.onChange(function() {
+            camera.position.x = -400;
+            camera.position.y = 200;
+            camera.position.z = 400;
+            //camera.rotation.y = -45 * Math.PI / 180 ;
+
+        });
+
+        dir_light.onChange(function() {
+            scene.remove(light2);
+            scene.add(light1);
+        });
+        spot_light.onChange(function() {
+            scene.remove(light1);
+            scene.add(light2);
+        });
+
+//        reset.onChange(function() {
+
+  //      });
 
     }
 
-
-
-
+    function clearScene() {
+        while(scene.children.length)
+            scene.remove(scene.children[0]);
+    }
 
     function render() {
         renderer.clear();
-        renderer.setClearColor(0xffefe0);
+        renderer.setClearColor(0xffffeb);
         renderer.render(scene, camera);
     }
 
